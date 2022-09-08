ba77b205edade23dd2180705c65d18d9d930ff9a
luksab <lukas@sabatschus.de>
2022-09-08 15:51:18 +0200

    temporarily remove vscode extensions

--- home-manager/modules/vscode/default.nix
+++ home-manager/modules/vscode/default.nix
@@ -21,37 +21,38 @@ in {
       extensions = with pkgs.vscode-extensions;
         [
           # arrterian.nix-env-selector
-          eamodio.gitlens
-          bbenoist.nix
-          brettm12345.nixfmt-vscode
-          arrterian.nix-env-selector
-          ms-vscode-remote.remote-ssh
-          matklad.rust-analyzer
-          serayuzgur.crates
-          (lib.mkIf (config.luksab.arch == "x86_64") pkgs.vsliveshare-new)
-          james-yu.latex-workshop
-          valentjn.vscode-ltex
-          (lib.mkIf (config.luksab.arch == "x86_64") ms-python.python)
+          #   eamodio.gitlens
+          #   bbenoist.nix
+          #   brettm12345.nixfmt-vscode
+          #   arrterian.nix-env-selector
+          #   ms-vscode-remote.remote-ssh
+          #   matklad.rust-analyzer
+          #   serayuzgur.crates
+          #   (lib.mkIf (config.luksab.arch == "x86_64") pkgs.vsliveshare-new)
+          #   james-yu.latex-workshop
+          #   valentjn.vscode-ltex
+          #   (lib.mkIf (config.luksab.arch == "x86_64") ms-python.python)
 
-          (lib.mkIf (config.luksab.arch == "x86_64") ms-vscode.cpptools)
-          esbenp.prettier-vscode
-          tamasfe.even-better-toml
-        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-          name = "errorlens";
-          publisher = "usernamehw";
-          version = "3.5.1";
-          sha256 = "sha256-DjmCjwX6gEtloPZKxpTl485FacRpsfiVP0ZnCUteq58=";
-        }] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-          name = "liveserver";
-          publisher = "ritwickdey";
-          version = "5.7.5";
-          sha256 = "sha256-rKe746kwXoJx46K7+beBrjTPUCt8IgyeGg7okCW60ik=";
-        }] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-          name = "remote-containers";
-          publisher = "ms-vscode-remote";
-          version = "0.240.0";
-          sha256 = "sha256-/GcWLTC4aXN+9Ld85szfW8V+PKcxE+qZOX7OrXHCqrM=";
-        }];
+          #   (lib.mkIf (config.luksab.arch == "x86_64") ms-vscode.cpptools)
+          #   esbenp.prettier-vscode
+          #   tamasfe.even-better-toml
+          # ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+          #   name = "errorlens";
+          #   publisher = "usernamehw";
+          #   version = "3.5.1";
+          #   sha256 = "sha256-DjmCjwX6gEtloPZKxpTl485FacRpsfiVP0ZnCUteq58=";
+          # }] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+          #   name = "liveserver";
+          #   publisher = "ritwickdey";
+          #   version = "5.7.5";
+          #   sha256 = "sha256-rKe746kwXoJx46K7+beBrjTPUCt8IgyeGg7okCW60ik=";
+          # }] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+          #   name = "remote-containers";
+          #   publisher = "ms-vscode-remote";
+          #   version = "0.240.0";
+          #   sha256 = "sha256-/GcWLTC4aXN+9Ld85szfW8V+PKcxE+qZOX7OrXHCqrM=";
+          # }
+        ];
     };
   };
 }
